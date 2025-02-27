490967f4a1501387b6dbc0ed79c7921d0b14c2a6
Abu Bakkar Siddique <157052083+SoloDevAbu@users.noreply.github.com>
2025-02-27 23:42:23 +0530

    added simple developer dashboard

--- packages/ui/src/appbar.tsx
+++ packages/ui/src/appbar.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "./button";
+import { useRouter } from "next/navigation";
 
 export const Appbar = () => {
   const { data: session } = useSession();
+  const router = useRouter();
   return (
     <div className="fixed top-0 left-0 right-0 flex justify-between border-b px-4 py-4 items-center mx-4 rounded-md shadow-sm shadow-gray-500 mt-2 bg-white z-50">
       <div className="text-lg flex flex-col justify-center">
@@ -14,13 +16,30 @@ export const Appbar = () => {
         <a href="/about">Privacy Policy</a>
         <a href="/about">Guidelines</a>
       </div>
-      <div className="flex flex-col justify-center">
-        <Button
-          className="bg-gray-800 hover:bg-gray-700 cursor-pointer text-white px-2 py-1 rounded-md"
-          onClick={session ? () => signOut() : () => signIn()}
-        >
-          {session ? "Logout" : "Login"}
-        </Button>
+      <div className="flex gap-2 justify-center">
+        {session ? (
+          <div className="flex gap-2 justify-center">
+            <Button
+              className="bg-gray-800 hover:bg-gray-700 cursor-pointer text-white px-2 py-1 rounded-md"
+              onClick={() => router.push("/developer/dashboard")}
+            >
+              Dashboard
+            </Button>
+            <Button
+              className="bg-gray-800 hover:bg-gray-700 cursor-pointer text-white px-2 py-1 rounded-md"
+              onClick={() => signOut()}
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            className="bg-gray-800 hover:bg-gray-700 cursor-pointer text-white px-2 py-1 rounded-md"
+            onClick={() => signIn()}
+          >
+            Login
+          </Button>
+        )}
       </div>
     </div>
   );
