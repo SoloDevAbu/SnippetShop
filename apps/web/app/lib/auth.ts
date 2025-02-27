63cf3a060a32922ea146ecaa30fcfe05cf4f6217
Abu Bakkar Siddique <157052083+SoloDevAbu@users.noreply.github.com>
2025-02-28 00:15:16 +0530

    Make the authOption inside lib folder typescript types fixes

--- apps/web/app/lib/auth.ts
+++ apps/web/app/lib/auth.ts
@@ -1,56 +1,74 @@
 import db from "@repo/db/client";
-import bcrypt from 'bcrypt';
+import { Account, Profile, User } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID || "",
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || ""
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         })
     ],
     callbacks: {
-        async signIn({ user, account, profile }: { user: any, account: any, profile: any }) {
-            if (account.provider === 'github') {
-                const existingUser = await db.user.findUnique({
-                    where: { provider_id: profile.id }
-                });
+        async signIn({ account, profile }:
+            {
+                user?: User | null, account: Account | null, profile?: Profile | null
 
-                if (!existingUser) {
-                    await db.user.create({
-                        data: {
-                            email: profile.email,
-                            name: profile.name,
-                            role: 'DEVELOPER',
-                            provider: 'GITHUB',
-                            provider_id: profile.id,
+            }) {
+                try {
+                    if(!account || !profile) return false;
+                    if (account.provider === 'github') {
+                        const githubProfile = profile as {id?: number; email?: string; name?: string};
+
+                        if(!githubProfile.id) return false;
+                        const existingUser = await db.user.findUnique({
+                            where: { provider_id: githubProfile.id }
+                        });
+        
+                        if (!existingUser) {
+                            await db.user.create({
+                                data: {
+                                    email: githubProfile.email as string,
+                                    name: githubProfile.name,
+                                    role: 'DEVELOPER',
+                                    provider: 'GITHUB',
+                                    provider_id: githubProfile.id,
+                                }
+                            });
                         }
-                    });
-                }
-            } else if (account.provider === 'google') {
-                const existingUser = await db.user.findUnique({
-                    where: { email: profile.email }
-                });
-                // console.log("Google Profile Data:", JSON.stringify(profile, null, 2));
+                    } else if (account.provider === 'google') {
+                        const googleProfile = profile as {email?: string; name?: string};
 
+                        if(!googleProfile.email) return false;
 
-                if (!existingUser) {
-                    await db.user.create({
-                        data: {
-                            email: profile.email,
-                            name: profile.name,
-                            role: 'USER',
-                            provider: 'GOOGLE',
+                        const existingUser = await db.user.findUnique({
+                            where: { email: googleProfile.email }
+                        });
+                        // console.log("Google Profile Data:", JSON.stringify(profile, null, 2));
+        
+        
+                        if (!existingUser) {
+                            await db.user.create({
+                                data: {
+                                    email: googleProfile.email,
+                                    name: googleProfile.name,
+                                    role: 'USER',
+                                    provider: 'GOOGLE',
+                                }
+                            });
                         }
-                    });
+                    }
+                    return true;
+                    
+                } catch (error) {
+                    console.error("Authentication error:", error);
+                    return false;
                 }
-            }
-            return true;
         }
     },
     secret: process.env.NEXTAUTH_SECRET || "secret"
