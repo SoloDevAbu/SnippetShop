ab62cb9b9918a1b51a8af5cb971b7cf8026ea0b7
Abu Bakkar Siddique <157052083+SoloDevAbu@users.noreply.github.com>
2025-03-05 15:47:05 +0530

    made theme to dark

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../providers";
 import { JSX } from "react/jsx-runtime";
+import { ThemeProvider } from "@repo/ui/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <Providers>
         <body className={inter.className}>
-          <div className="">
-            {children}
-          </div>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+            enableSystem
+
+          >
+            <div className="">
+              {children}
+            </div>
+          </ThemeProvider>
         </body>
       </Providers>
     </html>
